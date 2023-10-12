0b6df02920065ae18f59670908fd55131cbc82f7
cherhy <ekxk1234@naver.com>
2023-10-12 23:21:59 +0900

    api 실행 테스트

---
+++ src/main/kotlin/dev/fastcampus/webfluxcoroutine/controller/HomeController.kt
@@ -0,0 +1,26 @@
+package dev.fastcampus.webfluxcoroutine.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @GetMapping("/")
+    suspend fun index(): String {
+        return "main page"
+    }
+
+    @GetMapping("/hello/{name}")
+    suspend fun hello(@PathVariable name: String): String {
+        return "Hello $name"
+    }
+
+    @GetMapping("/hello")
+    suspend fun helloParam(@RequestParam name: String?): String {
+        return "Hello $name"
+    }
+
+}
