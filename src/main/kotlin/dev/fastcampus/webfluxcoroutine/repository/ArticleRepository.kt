5950a1da9c8edd3e378a4f54eb3e60cf2c8ab714
cherhy <ekxk1234@naver.com>
2023-10-13 16:54:15 +0900

    CRUD API 생성

--- src/main/kotlin/dev/fastcampus/webfluxcoroutine/repository/ArticleRepository.kt
+++ src/main/kotlin/dev/fastcampus/webfluxcoroutine/repository/ArticleRepository.kt
@@ -1,11 +1,9 @@
 package dev.fastcampus.webfluxcoroutine.repository
 
 import dev.fastcampus.webfluxcoroutine.model.Article
-import org.springframework.data.r2dbc.repository.R2dbcRepository
-import org.springframework.stereotype.Repository
-import reactor.core.publisher.Flux
+import kotlinx.coroutines.flow.Flow
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-@Repository
-interface ArticleRepository: R2dbcRepository<Article, Long> {
-    suspend fun findAllByTitleContains(title: String): Flux<Article>
+interface ArticleRepository: CoroutineCrudRepository<Article, Long> {
+    suspend fun findAllByTitleContains(title: String): Flow<Article>
 }
