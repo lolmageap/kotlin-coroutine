e5538a7c1157289bfefa526425d0933b29a38871
cherhy <ekxk1234@naver.com>
2023-10-13 16:53:10 +0900

    kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,24 +19,37 @@ repositories {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.7.2")
+    implementation("io.micrometer:context-propagation:1.0.3")
 
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
+//	implementation("io.github.numichi:reactive-logger:5.1.4")
+
+    implementation("io.github.resilience4j:resilience4j-kotlin:2.0.0")
+    implementation("io.github.resilience4j:resilience4j-all:2.0.0")
+
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-
-    runtimeOnly("org.mariadb:r2dbc-mariadb:1.1.3")
+    runtimeOnly("org.mariadb:r2dbc-mariadb")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 
-    testRuntimeOnly("com.h2database:h2")
-    testRuntimeOnly("io.r2dbc:r2dbc-h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 }
 
 tasks.withType<KotlinCompile> {
