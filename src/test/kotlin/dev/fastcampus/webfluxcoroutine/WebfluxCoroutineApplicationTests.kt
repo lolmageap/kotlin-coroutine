851a8d7eb4bebf3f65bdaabec99aa93dd4cdcb65
cherhy <ekxk1234@naver.com>
2023-10-13 16:53:23 +0900

    실행 테스트

--- src/test/kotlin/dev/fastcampus/webfluxcoroutine/WebfluxCoroutineApplicationTests.kt
+++ src/test/kotlin/dev/fastcampus/webfluxcoroutine/WebfluxCoroutineApplicationTests.kt
@@ -1,12 +1,22 @@
 package dev.fastcampus.webfluxcoroutine
 
-import org.junit.jupiter.api.Test
+import dev.fastcampus.webfluxcoroutine.model.Article
+import dev.fastcampus.webfluxcoroutine.repository.ArticleRepository
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-class WebfluxCoroutineApplicationTests {
-
-    @Test
-    fun contextLoads() {
+@ActiveProfiles("test")
+class WebfluxCoroutineApplicationTests(
+    @Autowired private val repository: ArticleRepository,
+): StringSpec({
+    "contextLoads".config(false) {
+        val prev = repository.count()
+        repository.save( Article(title = "title1") )
+        val curr = repository.count()
+        curr shouldBe prev + 1
     }
-}
+})
