c92e177780823b803dbf923f0ae2dcaadc78db26
cherhy <ekxk1234@naver.com>
2023-12-09 23:57:10 +0900

    remove codes

--- src/main/kotlin/dev/fastcampus/webfluxcoroutine/repository/ArticleRepository.kt
+++ src/main/kotlin/dev/fastcampus/webfluxcoroutine/repository/ArticleRepository.kt
@@ -1,9 +1,9 @@
-package dev.fastcampus.webfluxcoroutine.repository
-
-import dev.fastcampus.webfluxcoroutine.model.Article
-import kotlinx.coroutines.flow.Flow
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-
-interface ArticleRepository: CoroutineCrudRepository<Article, Long> {
-    suspend fun findAllByTitleContains(title: String): Flow<Article>
-}
+//package dev.fastcampus.webfluxcoroutine.repository
+//
+//import dev.fastcampus.webfluxcoroutine.model.Article
+//import kotlinx.coroutines.flow.Flow
+//import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+//
+//interface ArticleRepository: CoroutineCrudRepository<Article, Long> {
+//    suspend fun findAllByTitleContains(title: String): Flow<Article>
+//}
