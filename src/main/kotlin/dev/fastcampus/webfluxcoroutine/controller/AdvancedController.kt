7e655ceb9bc8a04a3192ec78765f5715c9d72d92
lolmageap <ekxk1234@naver.com>
2023-12-09 22:54:27 +0900

    add with context

--- src/main/kotlin/dev/fastcampus/webfluxcoroutine/controller/AdvancedController.kt
+++ src/main/kotlin/dev/fastcampus/webfluxcoroutine/controller/AdvancedController.kt
@@ -1,6 +1,9 @@
 package dev.fastcampus.webfluxcoroutine.controller
 
 import dev.fastcampus.webfluxcoroutine.service.AdvancedService
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.slf4j.MDCContext
+import kotlinx.coroutines.withContext
 import mu.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,9 +16,12 @@ class AdvancedController(
 ) {
     @GetMapping("/test/mdc")
     suspend fun testRequestTxId() {
-        logger.debug { "Hello MDC TxId start" }
-        advancedService.mdc()
-        logger.debug { "Hello MDC TxId end" }
+        withContext(MDCContext()) {
+            logger.debug { "Hello MDC TxId start" }
+            delay(100)
+            advancedService.mdc()
+            logger.debug { "Hello MDC TxId end" }
+        }
     }
 
 }
