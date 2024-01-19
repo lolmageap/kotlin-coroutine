ad7859ed0df2281d34a993420dd1c6a531e2ec53
cherhy <ekxk1234@naver.com>
2024-01-19 23:59:58 +0900

    fix to find logic and query extension method

--- src/main/kotlin/dev/fastcampus/webfluxcoroutine/service/ArticleService.kt
+++ src/main/kotlin/dev/fastcampus/webfluxcoroutine/service/ArticleService.kt
@@ -1,26 +1,25 @@
 package dev.fastcampus.webfluxcoroutine.service
 
 import dev.fastcampus.webfluxcoroutine.exception.ArticleNotFound
-import dev.fastcampus.webfluxcoroutine.model.Article
-import dev.fastcampus.webfluxcoroutine.model.CreateArticle
-import dev.fastcampus.webfluxcoroutine.model.UpdateArticle
-import dev.fastcampus.webfluxcoroutine.model.toEntity
+import dev.fastcampus.webfluxcoroutine.model.*
 import dev.fastcampus.webfluxcoroutine.repository.ArticleRepository
+import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Service
 
 @Service
 class ArticleService(
     private val articleRepository: ArticleRepository,
+    private val databaseClient: DatabaseClient,
 ) {
 
-    suspend fun create(createArticle: CreateArticle) = articleRepository.save( createArticle.toEntity() )
+    suspend fun create(createArticle: CreateArticle) = articleRepository.save(createArticle.toEntity())
 
-    suspend fun get(id: Long) : Article = articleRepository.findById(id)
+    suspend fun get(id: Long): Article = articleRepository.findById(id)
         ?: throw ArticleNotFound("No article found (id: $id)")
 
-    suspend fun getAll(title: String? = null) =
-        if ( title.isNullOrBlank() ) articleRepository.findAll()
-        else articleRepository.findAllByTitleContains(title)
+//    suspend fun getAll(title: String? = null) =
+//        if ( title.isNullOrBlank() ) articleRepository.findAll()
+//        else articleRepository.findAllByTitleContains(title)
 
     suspend fun update(id: Long, updateArticle: UpdateArticle): Article {
         val article = articleRepository.findById(id)
@@ -33,6 +32,37 @@ class ArticleService(
         }
     }
 
+    suspend fun getAll(request: QueryArticle) {
+        val param = HashMap<String, Any>()
+        val sql = databaseClient.sql {
+            """
+            SELECT * FROM ARTICLE
+            WHERE 1=1
+            ${
+                request.title.query { title ->
+                    param["title"] = title.trim().let { "%$it%" }
+                    "AND title LIKE :title"
+                }
+            }
+            ${
+                request.authorId.query {
+                    param["authorId"] = it
+                    "AND author_id in (:authorId)"
+                }
+            }
+            """.trimIndent()
+        }
+    }
+
     suspend fun delete(id: Long) = articleRepository.deleteById(id)
 
 }
+
+fun <T> T?.query(f: (T) -> String) =
+    when {
+        this == null -> ""
+        this is String && this.isBlank() -> ""
+        this is Collection<*> && this.isEmpty() -> ""
+        this is Array<*> && this.isEmpty() -> ""
+        else -> f.invoke(this)
+    }
